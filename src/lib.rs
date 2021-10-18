4466971a3386548b3b91587b52cc4c6495580cba
Tim Crawford <tcrawford@system76.com>
2021-10-18 11:38:31 -0600

    Replace llvm_asm with asm
    llvm_asm is deprecrated and may be removed. Replace it with the new asm.
    
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 #![cfg_attr(not(feature = "stable"), no_std)]
-#![cfg_attr(all(not(feature = "stable"), any(target_arch = "x86", target_arch = "x86_64")), feature(llvm_asm))]
+#![cfg_attr(all(not(feature = "stable"), any(target_arch = "x86", target_arch = "x86_64")), feature(asm))]
 #![cfg_attr(not(feature = "stable"), feature(const_fn_trait_bound))]
 
 pub use self::io::*;
